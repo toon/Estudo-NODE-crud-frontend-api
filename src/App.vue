08e5da853575c057f37ec4ead2b3c837017c22e3
toon <igor.mendonca@gmail.com>
2024-08-30 12:02:29 -0300

    Inclusão do v-data-table

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,44 @@
 <template>
-  <v-app>
-    <v-main>
-      <router-view />
+  <v-app id="inspire">
+    <!-- <v-system-bar>
+      <v-spacer></v-spacer>
+
+      <v-icon>mdi-square</v-icon>
+
+      <v-icon>mdi-circle</v-icon>
+
+      <v-icon>mdi-triangle</v-icon>
+    </v-system-bar> -->
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Application</v-app-bar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      temporary
+    >
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-main class="bg-grey-lighten-2">
+      <v-container>
+        <router-view />
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
-<script>
-export default {
-  name: "App",
+<script setup>
+  import { ref } from 'vue'
 
-  data: () => ({
-    //
-  }),
-};
+  const drawer = ref(null)
+</script>
+
+<script>
+  export default {
+    data: () => ({ drawer: null }),
+  }
 </script>
--- src/views/ParMoedaListView.vue
+++ src/views/ParMoedaListView.vue
@@ -1,4 +1,275 @@
+<!-- eslint-disable vue/valid-v-slot -->
 <template>
+  <v-data-table
+    :headers="headers"
+    :items="parmoedas"
+    :sort-by="[{ key: 'name', order: 'asc' }]"
+  >
+    <template v-slot:top>
+      <v-toolbar
+        flat
+      >
+        <v-toolbar-title>Pares de moeda</v-toolbar-title>
+        <v-divider
+          class="mx-4"
+          inset
+          vertical
+        ></v-divider>
+        <v-spacer></v-spacer>
+        <v-dialog
+          v-model="dialog"
+          max-width="500px"
+        >
+          <template v-slot:activator="{ props }">
+            <v-btn
+              class="mb-2"
+              color="primary"
+              dark
+              v-bind="props"
+            >
+              Novo item
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">{{ formTitle }}</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col
+                    cols="12"
+                    md="4"
+                    sm="6"
+                  >
+                    <v-text-field
+                      v-model="editedItem.name"
+                      label="Dessert name"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    md="4"
+                    sm="6"
+                  >
+                    <v-text-field
+                      v-model="editedItem.calories"
+                      label="Calories"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    md="4"
+                    sm="6"
+                  >
+                    <v-text-field
+                      v-model="editedItem.fat"
+                      label="Fat (g)"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    md="4"
+                    sm="6"
+                  >
+                    <v-text-field
+                      v-model="editedItem.carbs"
+                      label="Carbs (g)"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    md="4"
+                    sm="6"
+                  >
+                    <v-text-field
+                      v-model="editedItem.protein"
+                      label="Protein (g)"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="blue-darken-1"
+                variant="text"
+                @click="close"
+              >
+                Cancel
+              </v-btn>
+              <v-btn
+                color="blue-darken-1"
+                variant="text"
+                @click="save"
+              >
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue-darken-1" variant="text" @click="closeDelete">Cancel</v-btn>
+              <v-btn color="blue-darken-1" variant="text" @click="deleteItemConfirm">OK</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:item.actions="{ item }">
+      <v-icon
+        class="me-2"
+        size="small"
+        @click="editItem(item)"
+      >
+        mdi-pencil
+      </v-icon>
+      <v-icon
+        size="small"
+        @click="deleteItem(item)"
+      >
+        mdi-delete
+      </v-icon>
+    </template>
+    <template v-slot:item.status="{ value }">
+      <v-chip :color="getColor(value)">
+        {{ status_text }}
+      </v-chip>
+    </template>    
+    <template v-slot:no-data>
+      <v-btn
+        color="primary"
+        @click="loadItems"
+      >
+        Recarregar
+      </v-btn>
+    </template>
+  </v-data-table>
+</template>
+<script>
+import api from "../services/api";
+
+export default {
+  data: () => ({
+    parmoedas: [],
+    headers: [
+      { title: "Cód", value: "id" },
+      { title: "Par moeda", value: "name" },
+      { title: "Status", key: "status", value: "active" },
+      { title: "Ações", value: "actions", align: "end", sortable: false },
+    ],
+    dialog: false,
+    dialogDelete: false,
+    editedIndex: -1,
+    editedItem: {
+      name: '',
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0,
+    },
+    defaultItem: {
+      name: '',
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0,
+    },
+  }),
+
+  computed: {
+    formTitle () {
+      return this.editedIndex === -1 ? 'Novo item' : 'Editar item'
+    },
+    status_text () {
+      return this.value == 0 ? 'Ativo' : 'Inativo'
+    },
+  },
+
+  watch: {
+    dialog (val) {
+      val || this.close()
+    },
+    dialogDelete (val) {
+      val || this.closeDelete()
+    },
+  },
+
+  created() {
+    this.loadItems();
+  },
+
+  methods: {
+    getColor (status) {
+      return status == 0 ? 'red' : 'green'
+    },
+    loadItems() {
+    api.get("/parmoeda").then((response) => {
+        this.parmoedas = response.data;
+      });
+    },
+
+    // editItem(id) {
+    //   this.$router.push(`/edit/${id}`);
+    // },
+    // deleteItem(id) {
+    //   api.delete(`/parmoeda/${id}`).then(() => this.loadItems());
+    // },
+
+    editItem (item) {
+      this.editedIndex = this.desserts.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+
+    deleteItem (item) {
+      this.editedIndex = this.desserts.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialogDelete = true
+    },
+
+    deleteItemConfirm () {
+      this.desserts.splice(this.editedIndex, 1)
+      this.closeDelete()
+    },
+
+    close () {
+      this.dialog = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    closeDelete () {
+      this.dialogDelete = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    save () {
+      if (this.editedIndex > -1) {
+        Object.assign(this.desserts[this.editedIndex], this.editedItem)
+      } else {
+        this.desserts.push(this.editedItem)
+      }
+      this.close()
+    },
+  },
+}
+</script>
+
+<!-- <template>
   <v-container>
     <v-data-table :items="items" :headers="headers">
       <template #default="{ item }">
@@ -44,4 +315,4 @@ export default {
     },
   },
 };
-</script>
+</script> -->
